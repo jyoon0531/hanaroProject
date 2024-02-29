de350bd01b53350a352f32f2c0120f36fd12d4f4
jyoon0531 <chupki0531@gmail.com>
2024-02-29 17:39:23 +0900

    album list

--- hanaroProject/src/components/Album.tsx
+++ hanaroProject/src/components/Album.tsx
@@ -1,5 +1,19 @@
-const Album = () => {
-  return <></>;
+// type AlbumType = {
+//   albumId: number;
+//   id: number;
+//   title: string;
+//   url: string;
+//   thumbnailUrl: string;
+// };
+
+type Album = { userId: number; id: number; title: string };
+type Props = {
+  albumId?: number;
+  albumData?: Album;
+};
+
+const Album = ({ albumId, albumData }: Props) => {
+  return <>{/* <h1>{albumData}</h1> */}</>;
 };
 
 export default Album;
--- hanaroProject/src/components/Albums.tsx
+++ hanaroProject/src/components/Albums.tsx
@@ -1,7 +1,43 @@
+import { useEffect, useState } from 'react';
+import { useSession } from '../contexts/session-contexts';
+import { useFetch } from '../hooks/fetch';
+import Album from './Album';
+// import Album from './Album';
+
+const BASE_URL = 'https://jsonplaceholder.typicode.com';
+
+type AlbumsType = { userId: number; id: number; title: string };
+
 const Albums = () => {
+  const {
+    session: { loginUser },
+  } = useSession();
+  const [albums, setAlbums] = useState<AlbumsType[]>([]);
+  const { data } = useFetch<AlbumsType[]>({
+    url: `${BASE_URL}/albums?userId=${loginUser?.id}`,
+    dependencies: [loginUser?.id],
+    defaultData: albums,
+  });
+
+  useEffect(() => {
+    if (data) {
+      setAlbums(data);
+    }
+    console.log('🚀 ~ Albums ~ data:', data);
+  }, [data]);
+
   return (
     <>
-      <h1>albums!!!!!</h1>
+      <div className='flex'>
+        <div className='text-lg font-bold'>앨범 목록</div>
+        <button className='btn-default'>앨범 상세 보기</button>
+        {loginUser?.id}
+      </div>
+      <div>
+        {albums.map((album) => (
+          <Album key={album.id} albumData={album} />
+        ))}
+      </div>
     </>
   );
 };
